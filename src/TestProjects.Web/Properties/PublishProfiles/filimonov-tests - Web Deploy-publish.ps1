3adce60104057b2a82e0cce4e4a1ad2539ce8273
dexter <mail.for.regging@gmail.com>
2016-08-18 22:22:48 +0200

    initial commit

---
+++ src/TestProjects.Web/Properties/PublishProfiles/filimonov-tests - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '3a8e46b3-f989-4b3a-8683-94c9d6a86a43'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
---
+++ src/TestProjects.Web/Scripts/testProjectsApp.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('testProjectsApp', ['ui.router', 'cgBusy'])
+        .config(['$urlRouterProvider', '$stateProvider', configRoutes]);
+
+    function configRoutes($urlRouterProvider, $stateProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('/', {
+                url: '/',
+                templateUrl: 'description.html',
+            })
+            .state('image-search', {
+                url: '/image-search',
+                views: {
+                    '': {
+                        templateUrl: 'image-search/views/image-search-main.html',
+                        controller: 'imageSearchController',
+                        controllerAs: 'vm'
+                    },
+                    'results@image-search': {
+                        templateUrl: 'image-search/views/image-search-results.html'
+                    },
+                    'history@image-search': {
+                        templateUrl: 'image-search/views/image-search-history.html'
+                    }
+                }
+            });
+    }
+
+})();
